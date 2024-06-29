202404f5f01bcd7028cf1f6c7ea37ec46981bdc5
Raul-Bits <raulreboucas061@gmail.com>
2024-06-29 11:55:23 -0300

    Update projeto.py

--- Primeiro-projeto.py
+++ Primeiro-projeto.py
@@ -1,2 +1,11 @@
-print('Hello world')
-print('Hello world2')
+n = int(input('Vagas - '))
+m = int(input('Clientes - '))
+lista_vi = []
+vagas_disponiveis = 0
+
+for i in range (m):
+    vi = int(input('Vaga - '))
+    if vi not in lista_vi:
+        vagas_disponiveis += 1
+        lista_vi.append(vi)
+print('Vagas disponíveis:',vagas_disponiveis,'\nvagas fixas:',lista_vi) 
